764240ac706846206036743688653c9ee6e136ed
Kennejidio <118420755+Kennejidio@users.noreply.github.com>
2022-11-17 13:46:35 +0800

    Update user-functions.php

--- App/user-functions.php
+++ App/user-functions.php
@@ -172,12 +172,65 @@ if (isset($_GET['logout'])) {
 
 
 // call the login() function if register_btn is clicked
-if (isset($_POST['login_btn'])) {
-	login();
+if (isset($_POST['stud_login_btn'])) {
+	loginS();
+}
+
+if (isset($_POST['emp_login_btn'])) {
+	loginE();
 }
 
 // LOGIN USER
-function login(){
+
+// Login for students
+function loginS(){
+	global $db, $username, $errors;
+
+	// grap form values
+	$username = e($_POST['username']);
+	$password = e($_POST['password']);
+
+	// make sure form is filled properly
+	if (empty($username)) {
+		array_push($errors, "Username is required");
+	}
+	if (empty($password)) {
+		array_push($errors, "Password is required");
+	}
+
+	// attempt login if no errors on form
+	if (count($errors) == 0) {
+		$password = md5($password);
+
+		$query = "SELECT * FROM users WHERE username='$username' AND password='$password' LIMIT 1";
+		$results = mysqli_query($db, $query);
+
+		if (mysqli_num_rows($results) == 1) { // user found
+			// check if user is admin or user
+			$logged_in_user = mysqli_fetch_assoc($results);
+			if ($logged_in_user['user_type'] == 'student') {
+
+				$_SESSION['user'] = $logged_in_user;
+				$_SESSION['success']  = "You are now logged in";
+				header('location: student/home.php');		  
+			}
+			// elseif ($logged_in_user['user_type'] == 'employee'){
+
+			// 	$_SESSION['user'] = $logged_in_user;
+			// 	$_SESSION['success']  = "You are now logged in";
+			// 	header('location: employee/home.php');	
+			// }
+			else {
+				array_push($errors, "Wrong username/password combination");
+			}
+		}else {
+			array_push($errors, "Wrong username/password combination");
+		}
+	}
+}
+
+// Login for employees and admin
+function loginE(){
 	global $db, $username, $errors;
 
 	// grap form values
@@ -213,11 +266,9 @@ function login(){
 				$_SESSION['user'] = $logged_in_user;
 				$_SESSION['success']  = "You are now logged in";
 				header('location: employee/home.php');	
-			}else {
-				$_SESSION['user'] = $logged_in_user;
-				$_SESSION['success']  = "You are now logged in";
-
-				header('location: student/home.php');
+			}
+			else {
+				array_push($errors, "Wrong username/password combination");
 			}
 		}else {
 			array_push($errors, "Wrong username/password combination");
